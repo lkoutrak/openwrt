cb631ec766077a58733a401ef97994a5f7353798
Damien Mascord <tusker@tusker.org>
2026-04-16 00:02:10 +1000

    mpc85xx: xtm330: persist working FIT bootcmd on upgrade

--- target/linux/mpc85xx/base-files/lib/upgrade/platform.sh
+++ target/linux/mpc85xx/base-files/lib/upgrade/platform.sh
@@ -13,12 +13,21 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	watchguard,xtm330)
+		local fwenv="/tmp/fw_env.config.xtm330"
+
+		cat >"$fwenv" <<EOFW
+/dev/mtd4 0x0 0x10000 0x10000
+EOFW
+
+		FW_ENV_CONFIG="$fwenv" fw_setenv bootcmd 'nand read 0x1000000 0x20000 0x500000;bootm 0x1000000'
+		nand_do_upgrade "$1"
+		;;
 	hpe,msm460|\
 	ocedo,panda|\
 	sophos,red-15w-rev1|\
 	watchguard,firebox-t10|\
-	watchguard,firebox-t15|\
-	watchguard,xtm330)
+	watchguard,firebox-t15)
 		nand_do_upgrade "$1"
 		;;
 	*)
